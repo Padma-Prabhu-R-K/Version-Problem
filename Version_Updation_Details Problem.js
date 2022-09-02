866c5a4588f216f10eeeb230db9bd0a66b6942d6
Padma-Prabhu-R-K <padmaprabhurk@gmail.com>
2022-09-02 13:42:14 +0530

    Version Problem Javascript

--- Version_Updation_Details Problem.js
+++ Version_Updation_Details Problem.js
@@ -1,52 +1,281 @@
-var Versions =[
+var Versions = [
     {
-    Version_name : ['Windows 1.01', 'Windows 1.02', 'Windows 1.03', 'Windows 1.04', 'Windows 2.01', 'Windows 2.03', 'Windows 2.1', 'Windows 2.11', 'Windows 3.0', 'Windows 3.1', 'Windows NT 3.1', 'Windows 3.11', 'Windows 3.2', 'Windows NT 3.5', 'Windows NT 3.51', 'Windows 95', 'Windows NT 4.0', 'Windows 98', 'Windows 2000', 'Windows Me', 'Windows XP_Whistler', 'Windows XP_Freestyle', 'Windows XP_Harmony', 'Windows XP_Symphony', 'Windows XP_Emerald', 'Windows Vista', "Windows 7", 'Windows 8', 'Windows 8.1_Blue1', 'Windows 8.1_Blue2', 'Windows 10 version 1507', 'Windows 10 version 1511', 'Windows 10 version 1607', 'Windows 10 version 1703', 'Windows 10 version 1709', 'Windows 10 version 1803', 'Windows 10 version 1809', 'Windows 10 version 1903', 'Windows 10 version 1909', 'Windows 10 version 2004', 'Windows 10 version 20H2', 'Windows 10 version 21H1', 'Windows 10 version 21H2', 'Windows 10 version 22H2', 'Windows 11 version 21H2', 'Windows 11 version 22H2' ],
-    Release_Date : [11-20-1985, 05-14-1986, 08-21-1986, 04-10-1987, 12-09-1987, 12-09-1987, 05-27-1988, 03-13-1989, 05-22-1990, 04-06-1992, 11-08-1993, 11-22-1993, 09-21-1994, 05-30-1995, 08-24-1995, 08-24-1996, 06-25-1998, 02-17-2000, 09-14-2000, 10-25-2001, 10-29-2002, 09-30-2003, 10-12-2004, 10-14-2005, 01-30-2007, 09-22-2009, 10-26-2012, 10-17-2013, 05-23-2014, 07-29-2015, 11-10-2015, 08-02-2016, 04-05-2017, 10-17-2017, 04-30-2018, 11-13-2018, 05-21-2019, 11-12-2019, 05-27-2020, 10-20-2020, 05-18-2021, 11-16-2021, H2-2022, 10-05-2021, H2-2022],
-    Bugs: [],
-    Features : ['Windows 1.0 .. clipboard, calendar', 
-                'Windows 1.02 .. clock, control panel', 
-                'Windows 1.03 .. clock, control panel, Notepad editor', 
-                'Windows 1.04 .. Windows Write and Windows Paint', 
-                'Windows 2.01 have Desktop icons and Expanded Memory', 
-                'Windows 2.03 have Improved graphics support,that one can overlap windows', 
-                'Windows 2.1 Controls the screen layout, and use keyboard shortcuts to speed up your work', 
-                'Windows 2.11.. More windows, more speed',
-                'Windows 3.0 has significantly better performance, advanced graphics with 16 colors, Program Manager, File Manager, and Print Manager arrive in Windows 3.0.',
-                'Windows 3.1 grows with the release of a new Windows software development kit (SDK), which helps software developers focus more on writing programs and less on writing device drivers',
-                'NT 3.1 is a 32-bit operating system, which makes it a strategic business platform that supports high-end engineering and scientific programs',
-                'Windows 3.11 adds peer-to-peer workgroup and domain networking support',
-                'Windows 3.2 included a DOS extender that broke the 1MB memory limit (a major breakthrough)',
-                'Windows NT 3.5 used in Workstations and server',
-                'Windows NT 3.51.. for the first time, PCs become an integral part of the emerging client/server computing evolution',
-                'Windows 95 features the first appearance of the Start menu, taskbar, and minimize, maximize, and close buttons on each window',
-                'Windows NT 4.0 enhanced multimedia capabilities, more powerful features for mobile computing, and integrated networking',
-                'Windows 98 include the ability to open and close programs more quickly, and support for reading DVD discs and universal serial bus (USB) devices, Also it is last version based on MS-DOS',
-                'Windows 2000 .. Professional simplifies hardware installation by adding support for a wide variety of new Plug and Play hardware, including advanced networking and wireless products, USB devices, IEEE 1394 devices, and infrared devices',
-                'Windows Me offers music, video, and home networking enhancements and Windows Movie Maker',
-                'Windows XP_Whistler.. It’s both fast and stable. Navigating the Start menu, taskbar, and Control Panel are more intuitive',
-                'Windows XP_Freestyle.. Designed for home use, Windows XP offers such enhancements as the Network Setup Wizard, Windows Media Player, Windows Movie Maker, and enhanced digital photo capabilities',
-                'Windows XP_Harmony.. designed for working with large amounts of memory and projects such as movie special effects, 3D animations, engineering, and scientific programs',
-                'Windows XP_Symphony.. features Home computing and Entertainment. You can browse the Internet, watch live television, enjoy digital music and video collections, and watch DVDs',
-                'Windows XP_Emerald..  remote desktop support, an encrypting file system, and system restore and advanced networking features',
-                'Windows Vista is Smart on security, also can watch television, view and send photographs, and edit videos',
-                'Windows 7 .. the wireless world has arrived, it’s common to get online at public wireless hotspots like coffee shops. Wireless networks can be created at the office or at home',
-                'Windows 8 .. The Start menu was replaced with a Start screen, Apps screen and Charms bar, the latter a small taskbar with five functions (search, share, start, devices and settings)',
-                'Windows 8.1 .. the latter a small taskbar with five functions (search, share, start, devices and settings)',
-                'Windows 10 version 1507 .. '],
-    type : ["patch",'patch','enghancement','enhancement','major','enhancement','patch','patch','major','patch','enhancement','patch','enhancement','enhancement','patch','patch','major','enhancement','patch','enhancement','major','major','patch','patch','major','patch','major','major','patch','enhancement','enhancement','major','major','patch','patch','patch','patch','major','enhancement','enhancement','enhancement','enhancement','patch','patch','enhancement','major','major'],
-    Author : []
-}
+        Name: 'Windows 1.01',
+        Released: new Date('11-20-1985'),
+        Version: '1.01',
+        Features: 'Windows 1.0 .. clipboard, calendar',
+        type: 'patch',
+        Bug: {
+            bug: 'GUI is not clear',
+            Bug_ID: '13121623'
+        } ,
+        Author: 'Bill Gates',
+        Architecture: 'x86-16'
+    },
+    {
+        Name: 'Windows 1.02',
+        Released: new Date('05-14-1986'),
+        Version: '1.02',
+        Features: 'Windows 1.02 .. clock, control panel',
+        type: 'patch',
+        Bug: {
+            bug: 'Ms-Dos is not suitable for higher bit servers',
+            Bug_ID: '21242325'
+        } ,
+        Author: 'William H. Gates III',
+        Architecture: 'x86-16'
+    },
+    {
+        Name: 'Windows 1.03',
+        Released: new Date('08-21-1986'),
+        Version: '1.03',
+        Features: 'Windows 1.03 .. clock, control panel, Notepad editor',
+        type: 'enhancement',
+        Bug: {
+            bug: 'not edible for personal computers and public use',
+            Bug_ID: '25367245'
+        } ,
+        Author: 'Steven A. Ballmer',
+        Architecture: 'x86-16'
+    },
+    {
+        Name: 'Windows 1.04',
+        Released: new Date('04-10-1987'),
+        Version: '1.04',
+        Features: 'Windows 1.04 .. Windows Write and Windows Paint',
+        type: 'enhancement',
+        Bug: {
+            bug: 'Lagging for many users to take to type',
+            Bug_ID: '25367265'
+        } ,
+        Author: 'Bradford L. Smith',
+        Architecture: 'x86-16'
+    },
+    {
+        Name: 'Windows 2.01',
+        Released: new Date('12-09-1987'),
+        Version: '2.01',
+        Features: 'Windows 2.01 have Desktop icons and Expanded Memory',
+        type: 'major',
+        Bug: {
+            bug: 'More Memory usage leads to Hanging of PC',
+            Bug_ID: '16231312'
+        } ,
+        Author: 'Raymond V. Gilmartin',
+        Architecture: 'x86-16, IA-32'
+    },
+    {
+        Name: 'Windows 2.03',
+        Released: new Date('12-09-1987'),
+        Version: '2.03',
+        Features: 'Windows 2.03 have Improved graphics support,that one can overlap windows',
+        type: 'enhancement',
+        Bug: {
+            bug: 'Overhanging lead to Formatting of Data',
+            Bug_ID: '23252124'
+        } ,
+        Author: 'William H. Gates III',
+        Architecture: 'x86-16, IA-32'
+    },
+    {
+        Name: 'Windows 2.1',
+        Released: new Date('05-27-1988'),
+        Version: '2.10',
+        Features: 'Windows 2.1 Controls the screen layout, and use keyboard shortcuts to speed up your work',
+        type: 'patch',
+        Bug: {
+            bug: 'Can work only in one windows',
+            Bug_ID: '23251242'
+        } ,
+        Author: 'Peter S. Klein',
+        Architecture: 'x86-16, IA-32'
+    },
+    {
+        Name: 'Windows 2.11',
+        Released: new Date('03-13-1989'),
+        Version: '2.11',
+        Features: 'Windows 2.11.. More windows, more speed',
+        type: 'patch',
+        Bug: {
+            bug: 'File Memory is not expensible',
+            Bug_ID: '23251243'
+        } ,
+        Author: 'Stephen J. Luczo',
+        Architecture: 'x86-16, IA-32'
+    },
+    {
+        Name: 'Windows 3.0',
+        Released: new Date('05-22-1990'),
+        Version: '3.00',
+        Features: 'Windows 3.0 has significantly better performance, advanced graphics with 16 colors, Program Manager, File Manager, and Print Manager arrive in Windows 3.0.',
+        type: 'major',
+        Bug: {
+            bug: 'Graphics might be slow some times',
+            Bug_ID: '23161213'
+        } ,
+        Author: 'Bill Gates',
+        Architecture: 'x86-16, IA-32'
+    },
+    {
+        Name: 'Windows 3.1',
+        Released: new Date('04-06-1992'),
+        Version: '3.10',
+        Features: 'Windows 3.1 grows with the release of a new Windows software development kit (SDK), which helps software developers focus more on writing programs and less on writing device drivers',
+        type: 'patch',
+        Bug: {
+            bug: 'Not suitable for Extensible users',
+            Bug_ID: '23161224'
+        },
+        Author: 'Bill Gates',
+        Architecture: 'x86-16, IA-32'
+    }
 ]
-console.log("Windows Versions:",Versions)
 
-patch = Versions.filter(arr => arr[0].type.includes("patch"))
-major = Versions.filter(arr => arr[0].type.includes("major"))
-enhancement = Versions.filter(arr =>arr[0].type.includes("enhancement"))
-console.log("Patch:",patch)
-console.log("major:",major)
-console.log("enhancement:",enhancement)
+// console.log('Versions:',Versions)
+
+console.log("------------------------------------------------------------------------------------------------------------------------------------------- ")
+array1 = []
+count = 0
 
 
+// 1.Function for No. of versions released on particular "YEAR"
+
+function findByYear(year) {
+    filteredYear = []
+    releasedYear = Versions.filter(Versions => (Versions.Released.toString().includes(year)))
+    console.log("Question 1: How many Versions released in the specified 'Year'?,  I/p: '1987' \n")
+
+    console.log(`Ans: '${releasedYear.length}' Versions have been found in the year '${year}' \n`)
+
+    for (p of releasedYear) {
+        count = 0
+        filteredYear.push(p.Name)
+        count++
+    }
+    console.log('Filter by Year:', filteredYear)
+    console.log("--------------------------------------------------------------------------------------------------------------------------------------- ")
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+// 2.Function for the "Architecture" which has been used for more versions
+
+function findArch(archi) {
+    filteredArchi = []
+    architech = Versions.filter(Versions => (Versions.Architecture.includes(archi)))
+    console.log("Question 2: Find the No. of Versions released based upon the specified 'Architecture',  I/p: 'IA-32' \n")
+
+    console.log(`Ans: '${architech.length}' versions are designed using based upon '${archi}' Architecture \n`)
+    for (i of architech) {
+        count = 0
+        filteredArchi.push(i.Name)
+        count++
+    }
+    console.log('Filtered by Architecure:', filteredArchi)
+    console.log("---------------------------------------------------------------------------------------------------------------------------------------- ")
+}
+
+// 3.Function for the versions which are based under the specified "TYPE"
+
+function findByType(Type) {
+    filteredType = []
+    typ = Versions.filter(Versions => Versions.type.includes(Type))
+    console.log("Question 3: Finding No. of Versions updated based on the specified 'type' \n")
+
+    console.log(`Ans: '${typ.length}' versions have been released in the type '${Type}' \n`)
+    for (i in typ) {
+        count = 0
+        filteredType.push(typ[i].Name)
+        count++
+    }
+    console.log('FilteredbyType:', filteredType)
+    console.log("--------------------------------------------------------------------------------------------------------------------------------------- ")
+}
+
+// 4.Function for the "Author" who contributed in more Versions 
+
+function findAuthor() {
+    array1 = []
+    for (i of Versions) {
+        count = 0
+        array1.push(i.Author)
+        count++
+    }
+
+    m = 1;
+    const Authorobj = {}
+    for (a of array1) {
+        if (a in Authorobj) {
+            Authorobj[a] += 1
+        }
+        else {
+            Authorobj[a] = 1
+        }
+    }
+
+    // Sorting the Object containing the Authors
+    const sort_des = (Obj) => {
+        arr4 = []
+        des = {}
+        for (m in Obj){
+               arr4.push(Obj[m]) 
+        }
+        
+        function srt1(a,b){
+               return a-b
+        }
+        arr4.sort(srt1)
+        arr4.reverse()
+        
+        for(n of arr4){
+           for(o in Obj){
+             if(n == Obj[o]){
+                des[o] = Obj[o]
+             }
+           }
+        }
+        return des
+    }
+
+    sorted_AuthorObj = sort_des(Authorobj)
+    console.log("Question 4: Which author has contributed in more Versions? \n")
+    for (b in sorted_AuthorObj) {
+        console.log(`Ans: '${b}' is the Author who has contibuted in more versions, contributing in '${sorted_AuthorObj[b]}' versions \n`)
+        break;
+    }
+
+    VersionByMostAuthor = Versions.filter(n => n.Author.includes('Bill Gates'))
+
+    filteredAuthor = []
+    for(r of VersionByMostAuthor){
+        filteredAuthor.push(r.Name)
+    }
+    console.log('Filtered by Author:',filteredAuthor)
+    console.log("------------------------------------------------------------------------------------------------------------------------------------- ")
+}
 
 
+// 5.Function for Bug_Id
 
+function findByBugid(id) {
+    console.log("Question 5: Finding Name of the version by the given i/p 'Bug_ID' \n")
+    // for(c in Versions){
+        array2=[]
+        // for(i of Versions){
+        //     array2.push(i.Bug.Bug_ID)
+        // }
+        // console.log(array2)
+    findbyID = Versions.filter(Obj => Obj.Bug.Bug_ID.includes(id))
+    array3=[]
+    for(c of findbyID){
+        console.log(`Ans: '${id}' belongs to the version '${c.Name}' \n`)
+    }  
+    console.log("----------------------------------------------------------------------------------------------------------------------------- ") 
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+
+findByYear('1987')
+findArch('IA-32')
+findByType('patch')
+findAuthor()
+findByBugid('23161224')
